2a945e65e72461b497646bc4eecb8c42d14ef81e
raji0704 <akularajeswari120@gmail.com>
2026-03-02 18:56:37 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,54 @@
+public class UC3_OOPS_Banner_Join {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                " ****** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "****** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *"
+        ));
+
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                "*       ",
+                " ***** "
+        ));
+    }
+}
